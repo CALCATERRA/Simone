711f6f38c16b615c8e4764765c5df2f6be15b859
CALCATERRA <91848379+CALCATERRA@users.noreply.github.com>
2025-03-28 11:47:34 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -19,24 +19,23 @@ def get_messengermessages():
     response = requests.get(url)
 
     if response.status_code == 200:
-        print("Messenger chiamata:")
         return response.json()
     else:
         print("Errore nel recupero dei messaggi Messenger:", response.text)
         return None
 
 # Funzione per ottenere i messaggi da Instagram
-def get_instagrammessages():
-    print("Funzione get_instagrammessages() chiamata!")
-    token = os.getenv("INSTAGRAM_TOKEN")
-    url = f"https://graph.instagram.com/v18.0/me/conversations?fields=messages{{message,from,id,created_time}}&access_token={token}"
-    response = requests.get(url)
+#def get_instagrammessages():
+ #   print("Funzione get_instagrammessages() chiamata!")
+  #  token = os.getenv("INSTAGRAM_TOKEN")
+   # url = f"https://graph.instagram.com/v18.0/me/conversations?fields=messages{{message,from,id,created_time}}&access_token={token}"
+    #response = requests.get(url)
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Errore nel recupero dei messaggi Instagram:", response.text)
-        return None
+    #if response.status_code == 200:
+     #   return response.json()
+    #else:
+      #  print("Errore nel recupero dei messaggi Instagram:", response.text)
+       # return None
 # Esempio di chiamata
 #print(get_instagrammessages())
         
